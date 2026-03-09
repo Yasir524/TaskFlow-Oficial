c147e77731c5d914e406d5aae49d83a6bff4e3e4
Yasir524 <yasirguzman98@gmail.com>
2026-03-08 22:08:43 -0600

    Add Firebase configuration to environment.ts
    Added Firebase configuration to the environment settings.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+     apiKey: "AIzaSyDz5fzmstKteUtefOviBNNzMcSx9RrzOcs",
+  authDomain: "taskflow-oficial.firebaseapp.com",
+  projectId: "taskflow-oficial",
+  storageBucket: "taskflow-oficial.firebasestorage.app",
+  messagingSenderId: "777187505380",
+  appId: "1:777187505380:web:e3067bd82d9b0dbf2da8f1",
+  measurementId: "G-G2F6NYS4NF"
+  }
 };
-
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
